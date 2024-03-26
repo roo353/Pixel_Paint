2b0446899ad9cc5b32a0558a4748dea69531051f
Kaitlyn Kinsey <kaitlynkinsey353@gmail.com>
2024-03-26 12:52:42 -0600

    Backgrounds
    Finished creating backgrounds for the Pixel Art scenes.

--- Pixel Paint Game/Assets/Scripts/BoxTouchBehavior.cs
+++ Pixel Paint Game/Assets/Scripts/BoxTouchBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class BoxTouchBehavior : MonoBehaviour
 {
     public SpriteRenderer filled;
+    
     public void OnMouseDown()
     {
         filled.color = ColorManager.currentColor;
@@ -13,7 +15,17 @@ public class BoxTouchBehavior : MonoBehaviour
         if (gameObject.tag == ColorManager.selectedTag)
         {
             ColorManager.numWrongRemaining -= 1;
+            GetComponent<Collider>().enabled = false;
             Debug.Log(ColorManager.numWrongRemaining);
+        }
+    }
+    
+    void SetTargetInvisible(Transform theTransform)
+    {                
+        foreach (Transform aaa in theTransform)
+        {
+            aaa.gameObject.GetComponent<Renderer>().enabled = false;
+            SetTargetInvisible(aaa);
         }
     }
 }
